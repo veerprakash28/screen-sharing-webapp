f2e5ca256f80bcf4ce383b21928fcc2d29839904
Veer Prakash <veer.prakash_cs.aiml19@gla.ac.in>
2024-02-02 01:21:03 +0530

    removed window

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ipcRenderer } = window.require("electron");
+const { ipcRenderer } = require("electron");
 const codeContainer = document.getElementById("codeContainer");
 const codeElement = document.getElementById("code");
 const startButton = document.getElementById("start");
